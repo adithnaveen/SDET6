27cf4be9d89ac78ce6612a8208153fddf9d0b6f3
Naveen <adith.naveen@gmail.com>
2017-06-22 19:53:44 +0530

    added one more line for testing

--- HelloWorld.java
+++ HelloWorld.java
@@ -7,5 +7,6 @@ package com.fannie;
 class HelloWorld{
 	public static void main(String [] args){
 		System.out.println("Welcome to Java");
+		System.out.println("This is another line for testing");
 	}
 }
